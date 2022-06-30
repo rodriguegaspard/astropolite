36805b59202029da730c2b35d526f656a45391ad
rodriguegaspard <rodrigue.gaspard@gmail.com>
2022-06-30 10:11:25 +0200

    Added Footer

--- client/src/components/ContactFooter.vue
+++ client/src/components/ContactFooter.vue
@@ -1,10 +1,43 @@
-<script>
+<script> 
 </script>
 
 <template>
   <footer>
+    <div class="socialmedia">
+      <h2>Rejoignez-nous sur nos réseaux sociaux</h2>
+      <div class="socialmedia">
+        <img alt="LinkedIn" src="../assets/LinkedIn.svg">
+        <img alt="Twitter" src="../assets/Twitter.svg">
+        <img alt="Discord" src="../assets/Discord.svg">
+      </div>
+    </div>
+    <div class="legalmentions">
+      <p>Mentions légales</p>
+      <img alt="Logo Visuel" src="../assets/Logo_Visuel.svg">
+      <p>Politique de confidentialité</p>
+    </div>
   </footer>
 </template>
 
 <style>
+  h2, h3 {
+    font-family: 'Montserrat', sans-serif;
+    color: white;
+  }
+
+  .socialmedia {
+    display: flex;
+    background-color: #FCA311;
+    justify-content: space-around;
+    flex-direction: row;
+    align-items: center;
+  }
+
+  .legalmentions {
+    display: flex;
+    background-color: #14213D;
+    justify-content: space-around;
+    flex-direction: row;
+    align-items: center;
+  }
 </style>
