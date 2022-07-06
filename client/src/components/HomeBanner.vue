0f4c4f7315600258d26641a6ee76a54722f6da5d
rodriguegaspard <rodrigue.gaspard@gmail.com>
2022-07-06 13:10:30 +0200

    Added a banner on the Home Page

---
+++ client/src/components/HomeBanner.vue
@@ -0,0 +1,42 @@
+<script>
+  import InputButton from "./InputButton.vue";
+
+  export default {
+  components: {
+    InputButton
+    }
+  }
+</script>
+
+<template>
+  <div class="gradient">
+    <h2 class="bannerText bannerTitle bannerElement">Découvrir, Rencontrer,<br>Partager</h2>
+    <h3 class="bannerText bannerElement">L'astronomie & l'astronautique à portée de main</h3>
+    <div class="bannerElement">
+      <InputButton name="Rejoignez-nous"/>
+    </div>
+  </div>
+</template>
+
+<style>
+  .gradient {
+    display: flex;
+    flex-direction: column;
+    background-image: linear-gradient(to right, white, white, gray);
+    padding: 50px;
+  }
+
+  .bannerText {
+    font-family: 'Montserrat' sans-serif;
+    color: #14213D;
+  }
+
+  .bannerTitle {
+    font-family: 'PinotGrigio' sans-serif;
+    font-size: 300%;
+  }
+
+  .bannerElement {
+    margin: 10px 50px;
+  }
+</style>
